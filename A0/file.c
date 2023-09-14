57907b40d0dbe3d493b097593b61d718aaeeabf8
VTcommander <emil.vedel.thage@gmail.com>
2023-09-14 13:44:44 +0200

    asdda

--- A0/file.c
+++ A0/file.c
@@ -2,19 +2,30 @@
 #include <stdlib.h> // exit, EXIT_FAILURE, EXIT_SUCCESS.
 #include <string.h> // strerror.
 #include <errno.h>  // errno.
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int print_error(char *path, int errnum) {
+  return fprintf(stdout, "%s: cannot determine (%s)\n",
+    path, strerror(errnum));
+}
 
 int main(int argc, char* argv[]) {
   int retval = EXIT_SUCCESS;
-  FILE * f;
+  FILE * file;
+  struct stat fileInfo;
 
   if (argc < 2) {
     int retval = EXIT_FAILURE;
     printf("Usage: file path\n");
     return retval;
   }
-  f = fopen(argv[argc-1], "r");
-  if (!f){
+  
+  file = fopen(argv[1], "r");
+  stat(argv[1], &fileInfo);
+  if (!file){
     retval = EXIT_FAILURE;
+    print_error(argv[1], retval);
     return retval;
   }
   return retval;
