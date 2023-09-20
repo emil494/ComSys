ae426b2b57c1f56095315eb9b8ee456dc2b6fbfb
NikolajK1405 <Nikolajkrarup@hotmail.com>
2023-09-20 22:17:32 +0200

    ASCII and ISO

--- A0/file.c
+++ A0/file.c
@@ -1,9 +1,10 @@
-#include <stdio.h>  // fprintf, stdout, stderr.
-#include <stdlib.h> // exit, EXIT_FAILURE, EXIT_SUCCESS.
-#include <string.h> // strerror.
-#include <errno.h>  // errno.
+#include <errno.h>   // errno.
+#include <stdio.h>   // fprintf, stdout, stderr.
+#include <stdlib.h>  // exit, EXIT_FAILURE, EXIT_SUCCESS.
+#include <string.h>  // strerror.
+#include <stdbool.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   // Check if 0 arguments were given
   if (argc == 1) {
     printf("Usage: file path\n");
@@ -28,20 +29,50 @@ int main(int argc, char* argv[]) {
   }
 
   // Loop trough the file, and find the char with the highest value
-  fseek(file, 0, SEEK_SET);
-  int higestChar = fgetc(file);
+  // fseek(file, 0, SEEK_SET);
+  // int higestChar = fgetc(file); // Delete, instead call isAsciiChar for every char
+  bool isAscii = true;
   for (int i = 0; i <= size; i++) {
     fseek(file, i, SEEK_SET);
     int currentChar = fgetc(file);
-    if (currentChar > higestChar)
-      higestChar = currentChar;
-  } 
+    if (!isAsciiChar(currentChar)) {
+      isAscii = false;
+    }
+  }
 
   // Check if the file is an ASCII text file
-  if (higestChar <= 127) {
+  if (isAscii) {
     printf("%s: ASCII text\n", argv[1]);
     return EXIT_SUCCESS;
   }
 
+  // Check if the file is an ISO-8859-1 text file
+  //if (higestChar <= 256) {
+  //  printf("%s: ISO-8859-1 text\n", argv[1]);
+  //  return EXIT_SUCCESS;
+  //}
+  // Check
+
   return EXIT_FAILURE;
+}
+
+bool isAsciiChar(int byte) {
+  if (byte >= 7 && byte <= 13) {
+    return true;
+  } else if (byte == 27) {
+    return true;
+  } else if (byte >= 32 && byte <= 126) {
+    return true;
+  }
+  return false;
+}
+
+bool isIsoChar(int byte) {
+  if (byte >= 160 && byte <= 255) {
+    return true;
+  } else if (isAsciiChar(byte)) {
+    return true;
+  } else {
+    return false;
+  }
 }
