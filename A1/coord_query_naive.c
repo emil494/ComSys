a576d17907c1ef342a834f45594ff85188360a27
simonsionsom <simon19653@gmail.com>
2023-10-01 14:10:25 +0200

    4.1

--- A1/coord_query_naive.c
+++ A1/coord_query_naive.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <errno.h>
 #include <assert.h>
+#include <math.h>
 
 #include "record.h"
 #include "coord_query.h"
@@ -15,18 +16,35 @@ struct naive_data {
 };
 
 struct naive_data* mk_naive(struct record* rs, int n) {
-  assert(0);
-  // TODO
+  struct naive_data *data = malloc(sizeof(struct naive_data));
+  assert(data != NULL);
+
+  data->n = n;
+  data->rs = rs;
+
+  return data;
 }
 
 void free_naive(struct naive_data* data) {
-  assert(0);
-  // TODO
+  free(data);
 }
 
+double euclid (double x1, double x2, double y1, double y2 ) {
+    return sqrt(pow(x1 - x2,2) + pow(y1 - y2,2));
+  }
+
 const struct record* lookup_naive(struct naive_data *data, double lon, double lat) {
-  assert(0);
-  // TODO
+  struct record* closest = &data->rs[0];
+  double closest_dist = euclid(lon,closest->lon,lat,closest->lat);
+
+  for (int i = 0; i < data->n; i++) {
+    double newDist = euclid(lon, data->rs[i].lon,lat, data->rs[i].lat);
+    if (newDist < closest_dist ){
+      closest = &data->rs[i];
+      closest_dist = newDist;
+    }  
+  }
+  return closest;
 }
 
 int main(int argc, char** argv) {
