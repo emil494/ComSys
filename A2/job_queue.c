7cb3781f223ea0fcef32f7f923530d67ac3bab5b
VTcommander <emil.vedel.thage@gmail.com>
2023-10-25 14:20:31 +0200

    working job_queue

--- A2/job_queue.c
+++ A2/job_queue.c
@@ -13,7 +13,6 @@ int job_queue_init(struct job_queue *job_queue, int capacity) {
 
   job_queue->n = capacity;
   job_queue->num = 0;
-
   job_queue->destroyed = false;
   pthread_mutex_init(&job_queue->lock, NULL);
   pthread_cond_init(&job_queue->cond_pop, NULL);
@@ -29,15 +28,10 @@ int job_queue_destroy(struct job_queue *job_queue) {
   while (job_queue->num != 0) {
     pthread_cond_wait(&job_queue->cond_destroy, &job_queue->lock);
   }
-
+  
+  pthread_cond_broadcast(&job_queue->cond_pop);
   pthread_mutex_unlock(&job_queue->lock);
-  pthread_mutex_destroy(&job_queue->lock);
-  pthread_cond_destroy(&job_queue->cond_destroy);
-  pthread_cond_destroy(&job_queue->cond_pop);
-  pthread_cond_destroy(&job_queue->cond_push);
-
   free(job_queue->buffer);
-  free(job_queue);
   return 0;
 }
 
@@ -47,6 +41,7 @@ int job_queue_push(struct job_queue *job_queue, void *data) {
   while (job_queue->num == job_queue->n && !job_queue->destroyed) {
     pthread_cond_wait(&job_queue->cond_push, &job_queue->lock);
   }
+
   job_queue->buffer[job_queue->num] = data;
   job_queue->num += 1;
 
