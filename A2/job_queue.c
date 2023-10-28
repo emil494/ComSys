35f4c37aa75fc0f9c682a4eb7618bd154a570b0e
VTcommander <emil.vedel.thage@gmail.com>
2023-10-28 15:19:06 +0200

    Revert "Merge pull request #10 from emil494/krarup"
    This reverts commit 7ba53e34349135ffe15fe9e9a5ec728c98e54160, reversing
    changes made to 89438d3e657f05f6e8235e064bdc50de4499ecbc.

--- A2/job_queue.c
+++ A2/job_queue.c
@@ -6,18 +6,14 @@
 
 
 int job_queue_init(struct job_queue *job_queue, int capacity) {
-  job_queue->buffer = malloc(capacity * sizeof(void*));
-
   if (job_queue->buffer == NULL || job_queue == NULL) {
-    free(job_queue->buffer);
     return 1;
   }
+  job_queue->buffer = malloc(capacity * sizeof(void*));
 
   job_queue->max = capacity;
   job_queue->num = 0;
-  job_queue->top = 0;
-  job_queue->rear = 0;
-
+  
   job_queue->destroyed = false;
   pthread_mutex_init(&job_queue->lock, NULL);
   pthread_cond_init(&job_queue->cond_pop, NULL);
@@ -29,7 +25,7 @@ int job_queue_init(struct job_queue *job_queue, int capacity) {
 int job_queue_destroy(struct job_queue *job_queue) {
   pthread_mutex_lock(&job_queue->lock);
 
-  while (job_queue->num > 0) {
+  while (job_queue->num != 0) {
     pthread_cond_wait(&job_queue->cond_destroy, &job_queue->lock);
   }
 
@@ -51,9 +47,8 @@ int job_queue_push(struct job_queue *job_queue, void *data) {
   while (job_queue->num == job_queue->max && !job_queue->destroyed) {
     pthread_cond_wait(&job_queue->cond_push, &job_queue->lock);
   }
-  
-  job_queue->buffer[job_queue->rear] = data;
-  job_queue->rear = (job_queue->rear + 1) % job_queue->max;
+
+  job_queue->buffer[job_queue->num] = data;
   job_queue->num += 1;
 
   pthread_cond_signal(&job_queue->cond_pop);
@@ -65,7 +60,7 @@ int job_queue_push(struct job_queue *job_queue, void *data) {
 int job_queue_pop(struct job_queue *job_queue, void **data) {
   pthread_mutex_lock(&job_queue->lock);
 
-  while (job_queue->num == 0 && !job_queue->destroyed) {
+  while (job_queue->num < 1 && !job_queue->destroyed) {
     pthread_cond_wait(&job_queue->cond_pop, &job_queue->lock);
   }
 
@@ -74,10 +69,15 @@ int job_queue_pop(struct job_queue *job_queue, void **data) {
     return -1;
   }
 
-  *data = job_queue->buffer[job_queue->top];
-  job_queue->buffer[job_queue->top] = NULL;
-  job_queue->top = (job_queue->top + 1) % job_queue->max;
+  *data = job_queue->buffer[0];
+  job_queue->buffer[0] = NULL;
   job_queue->num -= 1;
+  if (job_queue->num != 0) {
+    for (int i = 0; i <= job_queue->num; i++) {
+      job_queue->buffer[i] = job_queue->buffer[i+1];
+    }
+    job_queue->buffer[job_queue->num] = NULL;
+  }
 
   pthread_cond_signal(&job_queue->cond_push);
   pthread_cond_signal(&job_queue->cond_destroy);
