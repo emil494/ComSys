8bbfb9fd3e659930a3834deef8e31a58b8461536
NikolajK1405 <Nikolajkrarup@hotmail.com>
2023-10-29 15:51:22 +0100

    afleveret

---
+++ A2/src/fauxgrep-mt.c
@@ -0,0 +1,162 @@
+// Setting _DEFAULT_SOURCE is necessary to activate visibility of
+// certain header file contents on GNU/Linux systems.
+#define _DEFAULT_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+#include <stdbool.h>
+
+// err.h contains various nonstandard BSD extensions, but they are
+// very handy.
+#include <err.h>
+
+#include <pthread.h>
+
+#include "job_queue.h"
+
+pthread_mutex_t stdout_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct search_queue {
+  struct job_queue *queue;
+  char const *needle;
+};
+
+int fauxgrep_file(char const *needle, char const *path) {
+  FILE *f = fopen(path, "r");
+
+  if (f == NULL) {
+    warn("failed to open %s", path);
+    return -1;
+  }
+
+  char *line = NULL;
+  size_t linelen = 0;
+  int lineno = 1;
+
+  while (getline(&line, &linelen, f) != -1) {
+    if (strstr(line, needle) != NULL) {
+      assert(pthread_mutex_lock(&stdout_mutex) == 0);
+      printf("%s:%d: %s", path, lineno, line);
+      assert(pthread_mutex_unlock(&stdout_mutex) == 0);
+    }
+
+    lineno++;
+  }
+
+  free(line);
+  fclose(f);
+
+  return 0;
+}
+
+void* worker(void *arg) {
+  struct search_queue *sqp = arg;
+
+  while (1) {
+    char *path;
+    if (job_queue_pop(sqp->queue, (void**)&path) == 0) {
+      fauxgrep_file(sqp->needle, path);
+      free(path);
+    } else {
+      // If job_queue_pop() returned non-zero, that means the queue is
+      // being killed (or some other error occured).  In any case,
+      // that means it's time for this thread to die.
+      break;
+    }
+  }
+
+  return NULL;
+}
+
+int main(int argc, char * const *argv) {
+  if (argc < 2) {
+    err(1, "usage: [-n INT] STRING paths...");
+    exit(1);
+  }
+  int num_threads = 1;
+  char const *needle = argv[1];
+  char * const *paths = &argv[2];
+
+  if (argc > 3 && strcmp(argv[1], "-n") == 0) {
+    // Since atoi() simply returns zero on syntax errors, we cannot
+    // distinguish between the user entering a zero, or some
+    // non-numeric garbage.  In fact, we cannot even tell whether the
+    // given option is suffixed by garbage, i.e. '123foo' returns
+    // '123'.  A more robust solution would use strtol(), but its
+    // interface is more complicated, so here we are.
+    num_threads = atoi(argv[2]);
+
+    if (num_threads < 1) {
+      err(1, "invalid thread count: %s", argv[2]);
+    }
+
+    needle = argv[3];
+    paths = &argv[4];
+
+  } else {
+    needle = argv[1];
+    paths = &argv[2];
+  }
+
+  // Create job queue and search queue.
+  struct job_queue jq;
+  job_queue_init(&jq, 64);
+  struct search_queue sq;
+  struct search_queue *sqp = &sq;
+  sqp->queue = &jq;
+  sqp->needle = needle;
+  
+  // Start up the worker threads.
+  pthread_t *threads = calloc(num_threads, sizeof(pthread_t));
+  for (int i = 0; i < num_threads; i++) {
+    if (pthread_create(&threads[i], NULL, &worker, sqp) != 0) {
+      err(1, "pthread_create() failed");
+    }
+  }
+
+  // FTS_LOGICAL = follow symbolic links
+  // FTS_NOCHDIR = do not change the working directory of the process
+  //
+  // (These are not particularly important distinctions for our simple
+  // uses.)
+  int fts_options = FTS_LOGICAL | FTS_NOCHDIR;
+
+  FTS *ftsp;
+  if ((ftsp = fts_open(paths, fts_options, NULL)) == NULL) {
+    err(1, "fts_open() failed");
+    return -1;
+  }
+  
+  FTSENT *p;
+  while ((p = fts_read(ftsp)) != NULL) {
+    switch (p->fts_info) {
+    case FTS_D:
+      break;
+    case FTS_F: 
+      job_queue_push(sqp->queue, strdup(p->fts_path));
+      break;
+    default:
+      break;
+    }
+  }
+
+  fts_close(ftsp);
+
+   // Shut down the job queue and the worker threads here.
+  job_queue_destroy(sqp->queue);
+  // Wait for all threads to finish.  This is important, at some may still be working on their job.
+  for (int i = 0; i < num_threads; i++) {
+    if (pthread_join(threads[i], NULL) != 0) {
+      err(1, "pthread_join() failed");
+    }
+  }
+  free(threads);
+
+  return 0;
+}
