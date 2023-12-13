8b2be49e646ac12bc34a4c4faf3d5243da853e30
NikolajK1405 <Nikolajkrarup@hotmail.com>
2023-12-13 15:47:06 +0100

    1st try

---
+++ A5/src/simulate.c
@@ -0,0 +1,263 @@
+#include "simulate.h"
+
+// Simuler RISC-V program i givet lager og fra given start adresse
+long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file) {
+    int32_t regs[32];
+    int32_t pc = start_addr;
+    int32_t ins;
+    int32_t imm;
+    int32_t adr;
+    while(1) {
+        ins = memory_rd_w(mem, pc);
+
+        if (pc == NULL)
+            break;
+        pc += 4;
+        
+        int32_t opcode  = ins & 0x7f;
+        int32_t funct3  = (ins >> 12) & 0x7;
+        int32_t funct7  = (ins >> 25) & 0x7f;
+        
+        int rd  = (ins >> 7) & 0x1f;
+        int rs1 = (ins >> 15) & 0x1f;
+        int rs2 = (ins >> 20) & 0x1f;
+        regs[0] = 0;
+
+        imm = 0; // Reset variables
+        adr = 0;
+
+        switch (opcode)
+        {
+        case LUI:
+            regs[rd] =  ((ins >> 12) & 0xfffff) << 12;
+            break;
+        case AUIPC:
+            // TODO: make
+            break;
+        case JAL:
+            imm |= ((ins >> 21) & 0x3ff) << 1; 
+            imm |= ((ins >> 20) & 0x1) << 11;
+            imm |= ((ins >> 12) & 0xff) << 12;
+            imm |= ((ins >> 31) & 0x1) << 20;
+            regs[rd] = pc;
+            pc += imm - 4;
+            break;
+        case JALR:
+            imm = (ins >> 20) & 0xfff;
+            regs[rd] = pc;
+            pc = regs[rs1] + imm - 4;
+            break;
+        case ECALL:
+            // TODO: make
+            break;
+        case BRANCH:
+            imm |= ((ins >> 8) & 0xf) << 1;
+            imm |= ((ins >> 25) & 0x3f) << 5;
+            imm |= ((ins >> 7) & 0x1) << 11;
+            imm |= (ins >> 31) << 12;
+            imm -= 4; // Adjust for pc increment
+
+            switch (funct3)
+            {
+            case BEQ:
+                if(regs[rs1] == regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            case BNE:
+                if (regs[rs1] != regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            case BLT:
+                if (regs[rs1] < regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            case BGE:
+                if (regs[rs1] >= regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            case BLTU:
+                if ((uint32_t)regs[rs1] < (uint32_t)regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            case BGEU:
+                if ((uint32_t)regs[rs1] >= (uint32_t)regs[rs2]){
+                    pc += imm;
+                }
+                break;
+            default:
+                perror("BRANCH: Undefined funct3");
+                exit(1);
+                break;
+            }
+            break;
+        case LOAD:
+            adr = (ins >> 20) & 0xfff;
+            adr += regs[rs1];
+            switch (funct3)
+            {
+            case LB:
+                regs[rd] = memory_rd_b(mem, adr);
+                break;
+            case LH:
+                regs[rd] = memory_rd_h(mem, adr);
+                break;
+            case LW:
+                regs[rd] = memory_rd_w(mem, adr);
+                break;
+            case LBU:
+                regs[rd] = (uint32_t)memory_rd_b(mem, adr);
+                break;
+            case LHU:
+                regs[rd] = (uint32_t)memory_rd_h(mem, adr);
+                break;
+            default:
+                break;
+            }
+            break;
+        case STORE:
+            adr = ((ins >> 25) & 0x7f) << 5 | ((ins >> 7) & 0x1f);
+            adr += regs[rs1];
+            switch (funct3) {
+            case SB:
+                memory_wr_b(mem, adr, regs[rs2]);
+                break;
+            case SH:
+                memory_wr_h(mem, adr, regs[rs2]);
+                break;
+            case SW:
+                memory_wr_w(mem, adr, regs[rs2]);
+                break;
+            default:
+                break;
+            }
+            break;
+        case OP_IMM:
+            imm = (ins >> 20) & 0xFFF;
+            switch (funct3) {
+            case ADDI:
+                regs[rd] = regs[rs1] + imm;
+                break;
+            case SLLI:
+                regs[rd] = regs[rs1] << imm;
+                break;
+            case SLTI:
+                regs[rd] = (regs[rs1] < (int)imm) ? 1 : 0;
+                break;
+            case SLTIU:
+                regs[rd] = ((uint32_t)regs[rs1] < imm) ? 1 : 0;
+                break;
+            case XORI:
+                regs[rd] = regs[rs1] ^ imm;
+                break;
+            case SRI:
+                switch (funct7) {
+                case SRLI:
+                    regs[rd] = (uint32_t)regs[rs1] >> imm;
+                    break;
+                case SRAI:
+                    regs[rd] = regs[rs1] >> imm;
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case ORI:
+                regs[rd] = regs[rs1] | imm;
+                break;
+            case ANDI:
+                regs[rd] = regs[rs1] & imm;
+                break;
+            default:
+                break;
+            }
+            break;
+        case OP:
+            switch (funct3) {
+            case ADUB:
+                switch (funct7) {
+                case ADD:
+                    regs[rd] = regs[rs1] + regs[rs2];
+                    break;
+                case SUB:
+                    regs[rd] = regs[rs1] - regs[rs2];
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case SLL:
+                regs[rd] = regs[rs1] << regs[rs2];
+                break;
+            case SLT:
+                regs[rd] = (regs[rs1] < regs[rs2]) ? 1 : 0;
+                break;
+            case SLTU:
+                regs[rd] = (regs[rs1] < regs[rs2]) ? 1 : 0;
+                break;
+            case XOR:
+                regs[rd] = regs[rs1] ^ regs[rs2];
+                break;
+            case SR:
+                switch (funct7) {
+                case SRL:
+                    regs[rd] = regs[rs1] >> regs[rs2];
+                    break;
+                case SRA:
+                    regs[rd] = regs[rs1] >> regs[rs2];
+                    break;
+                default:
+                    break;
+                }
+                break;
+            case OR:
+                regs[rd] = regs[rs1] | regs[rs2];
+                break;
+            case AND:
+                regs[rd] = regs[rs1] & regs[rs2];
+                break;
+            default:
+                break;
+            }
+            break;
+        case RVM:
+            switch (funct3) {
+                case MUL:
+                    regs[rd] = regs[rs1] * regs[rs2];
+                    break;
+                case MULH:
+                    regs[rd] = (regs[rs1] * regs[rs2]) >> 16;
+                    break;
+                case MULHSU:
+                    regs[rd] = (regs[rs1] * regs[rs2]) >> 16;
+                    break;
+                case MULHU:
+                    regs[rd] = (regs[rs1] * regs[rs2]) >> 16;
+                    break;
+                case DIV:
+                    regs[rd] = regs[rs1] / regs[rs2];
+                    break;
+                case DIVU:
+                    regs[rd] = regs[rs1] / regs[rs2];
+                    break;
+                case REM:
+                    regs[rd] = regs[rs1] % regs[rs2];
+                    break;
+                case REMU:
+                    regs[rd] = regs[rs1] % regs[rs2];
+                    break;
+                default:
+                    break;
+            }
+        default:
+            perror("Undefined opcode");
+            exit(1);
+            break;
+        }
+    }
+    return 0;
+}
--- A5/src/simulate.h
+++ A5/src/simulate.h
@@ -4,6 +4,75 @@
 #include "memory.h"
 #include "assembly.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LUI     0x37
+#define AUIPC   0x17
+#define JAL     0x6f
+#define JALR    0x67
+#define ECALL   0x73
+
+#define BRANCH  0x63
+  #define BEQ     0x0
+  #define BNE     0x1
+  #define BLT     0x4
+  #define BGE     0x5
+  #define BLTU    0x6
+  #define BGEU    0x7
+
+#define LOAD    0x3
+  #define LB      0x0
+  #define LH      0x1
+  #define LW      0x2
+  #define LBU     0x4
+  #define LHU     0x5
+
+#define STORE   0x23
+  #define SB      0x0
+  #define SH      0x1
+  #define SW      0x2
+
+#define OP_IMM  0x13
+  #define ADDI    0x0
+  #define SLLI    0x1
+  #define SLTI    0x2
+  #define SLTIU   0x3
+  #define XORI    0x4
+  #define SRI     0x5
+    #define SRLI    0x0
+    #define SRAI    0x20
+  #define ORI     0x6
+  #define ANDI    0x7
+
+#define OP      0x33
+  #define ADUB    0x0
+    #define ADD     0x0
+    #define SUB     0x20
+  #define SLL     0x1
+  #define SLT     0x2
+  #define SLTU    0x3
+  #define XOR     0x4
+  #define SR      0x5
+    #define SRL     0x0
+    #define SRA     0x20
+  #define OR      0x6
+  #define AND     0x7
+
+//Check funct7 first, for simpler switch statement, as opcode for RV32M is same as OP
+#define RVM     0x1 
+  #define MUL     0x0
+  #define MULH    0x1
+  #define MULHSU  0x2
+  #define MULHU   0x3
+  #define DIV     0x4
+  #define DIVU    0x5
+  #define REM     0x6
+  #define REMU    0x7
+
+typedef struct register_state {
+  u_int32_t regs[32];
+  u_int32_t pc;
+} register_state_t;
 
 // Simuler RISC-V program i givet lager og fra given start adresse
 long int simulate(struct memory *mem, struct assembly *as, int start_addr, FILE *log_file);
